7ae4b811109ce739061ca44be3460b1dcb4a8bed
roryff <rory.fitzg@hotmail.com>
2024-09-02 13:17:27 +0200

    first test

--- Node1/main.c
+++ Node1/main.c
@@ -1,13 +1,19 @@
-#include <avr/io.h>
+#define F_CPU 4915200
 
+#include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
     // Initialize your microcontroller here
+    DDRA = 0xFF; // Set all pins of PORTA as output
 
     // Main loop
     while (1) {
         // Your code here
+        PORTA ^= 0b1; // Toggle the first pin of PORTA
+        _delay_ms(1); // Delay for 1 second
+
     }
 
-    return 0;
+    return 0; 
 }
