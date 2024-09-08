8b9cb5faba3b4d613a88523e00031d3b4babb3ad
heimsnik <cmheimvik@gmail.com>
2024-09-08 12:16:36 +0200

    New defines and new initXmem

--- Node1/defines.h
+++ Node1/defines.h
@@ -1,5 +1,19 @@
 #define F_CPU 4915200
 #define BAUDRATE 9600
-#define set_bit(reg, bit) (reg |= (1 << (bit)))
-#define clear_bit(reg, bit) (reg &= ~(1 << (bit)))
 #define test_bit(reg, bit) (reg & (1 << (bit)))
+
+#define clear_bit(reg, ...) \
+    do { \
+        int bits[] = {__VA_ARGS__}; \
+        for (int i = 0; i < sizeof(bits)/sizeof(bits[0]); ++i) { \
+            reg &= ~(1 << (bits[i])); \
+        } \
+    } while (0)
+
+#define set_bit(reg, ...) \
+    do { \
+        int bits[] = {__VA_ARGS__}; \
+        for (int i = 0; i < sizeof(bits)/sizeof(bits[0]); ++i) { \
+            reg |= (1 << (bits[i]));\
+        } \
+    } while (0)
