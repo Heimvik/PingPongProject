7a4a2e99c2096151049830f47565c7113a5bcf8b
Inge Grelland <inge@crazy4life.no>
2024-10-21 11:11:33 +0200

    whoops

--- Node2/drivers/gameLogic.c
+++ Node2/drivers/gameLogic.c
@@ -1,13 +1,13 @@
 #include "gameLogic.h"
 
 uint16_t goals = 0;
-uint64_t lastGoal = time_now();
+uint64_t lastGoal = 0;
 
 uint16_t checkAndReturnGoals()
 {
     if (readAdc() < 1069)
     {
-        if (time_now() - lastGoal >= MIN_GOAL_DELAY)
+        if (time_now() - lastGoal >= MIN_GOAL_DELAY || lastGoal = 0)
         {
             goals++;
         }
